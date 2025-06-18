232e39a626ebd6bfcbc7744dead0200b6df68cb5
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-06-18 10:00:48 +0530

    solved

---
+++ 1.Pattern/2.triangle.js
@@ -0,0 +1,23 @@
+/**
+ * Triangle Star Pattern
+ * for n = 5
+ *    *
+ *    * *
+ *    * * *
+ *    * * * *
+ *    * * * * *
+ */
+
+function trianglePattern(n) {
+  for (let row = 1; row <= n; row++) {
+    let star = '';
+
+    for (let col = 1; col <= row; col++) {
+      star += '* ';
+    }
+
+    console.log(star);
+  }
+}
+
+trianglePattern(5);
