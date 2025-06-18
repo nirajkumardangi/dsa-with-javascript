6eb7cfe7df06c925998328b8d8a4d625111b9b2c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-06-18 08:04:31 +0530

    Optimized Solution

--- 0.Basics/LoopingProblems/9.isPrime.js
+++ 0.Basics/LoopingProblems/9.isPrime.js
@@ -1,11 +1,11 @@
 // Q9. Check if a number is prime
 function isPrime(n) {
-  if (n < 1) {
-    return false;
-  }
+  if (n < 2) return false;
+  
   for (let i = 2; i < n; i++) {
     if (n % i === 0) return false;
   }
+
   return true;
 }
 
