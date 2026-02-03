eb17b3cd22fed3782dcc90e0aa1147269f08aa46
nirajkumardangi <niraj8825224435@gmail.com>
2026-02-03 14:39:11 +0530

    add delete first element example

--- Array/6.delete_first_element.js
+++ Array/6.delete_first_element.js
@@ -1,12 +1,11 @@
 // Delete First Element (without shift)
-const arr = [10, 20, 30, 40];
-
-for (let i = 0; i < arr.length - 1; i++) {
-  arr[i] = arr[i + 1];
+function deleteFromBeginning(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr.length = arr.length - 1;
+  return arr;
 }
 
-console.log(arr); // [ 20, 30, 40, 40 ]
-
-arr.length = arr.length - 1;
-
-console.log(arr); // [ 20, 30, 40 ]
+const result = deleteFromBeginning([10, 20, 30, 40]);
+console.log(result); // [ 20, 30, 40 ]
