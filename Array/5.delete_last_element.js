aa7e916a9c424edc2d85870fb9e45d829823830e
nirajkumardangi <niraj8825224435@gmail.com>
2026-02-03 14:38:07 +0530

    add delete last element example

--- Array/5.delete_last_element.js
+++ Array/5.delete_last_element.js
@@ -1,7 +1,8 @@
 // Delete Last Element (without pop)
+function deleteLastElement(arr) {
+  arr.length = arr.length - 1;
+  return arr;
+}
 
-const arr = [10, 20, 30, 40];
-
-arr.length = arr.length - 1;
-
-console.log(arr);
+const result = deleteLastElement([10, 20, 30, 40]);
+console.log(result);
