f6ce53f94b4a074ed1cd4825517170c9f2cbd85c
nirajkumardangi <niraj8825224435@gmail.com>
2026-09-05 20:46:01 +0530

    Add function to find max consecutive ones

---
+++ 2.Array/17.findMaxConsecutiveOnes.js
@@ -0,0 +1,20 @@
+// Leetcode Problem 485 : Max Consecutive Ones
+
+var findMaxConsecutiveOnes = function (nums) {
+  let currCount = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      currCount++;
+    } else {
+      maxCount = Math.max(currCount, maxCount);
+      currCount = 0;
+    }
+  }
+
+  return Math.max(maxCount, currCount);
+};
+
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // 3
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // 2
